bd42f9c5f6b86fdef7d6501388c0186c65406ec2
spandhana98 <74965732+spandhana98@users.noreply.github.com>
2021-06-15 11:03:49 +0530

    Add files via upload

---
+++ Home.js
@@ -0,0 +1,98 @@
+import React, { Component } from "react";
+import {
+    View,
+    Text,
+    StyleSheet,
+    SafeAreaView,
+    TouchableOpacity,
+    Platform,
+    StatusBar,
+    ImageBackground,
+    Image
+} from "react-native";
+
+export default class HomeScreen extends Component {
+    
+
+    render() {
+        return (
+            <View style={styles.container}>
+                <SafeAreaView style={styles.droidSafeArea} />
+                <ImageBackground source={require('../assets/stars.gif')} style={styles.backgroundImage}>
+                    <View style={styles.titleBar}>
+                        <Text style={styles.titleText}>Stellar App</Text>
+                    </View>
+                    <TouchableOpacity style={styles.routeCard} onPress={() =>
+                        this.props.navigation.navigate("SpaceCraft")
+                    }>
+                        <Text style={styles.routeText}>SpaceCraft</Text>
+                     
+                        <Image source={require("../assets/SpaceCraft.png")} style={styles.iconImage}></Image>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.routeCard} onPress={() =>
+                        this.props.navigation.navigate("StarMap")
+                    }>
+                        <Text style={styles.routeText}>Star Map</Text>
+                       
+                        <Image source={require("../assets/star_map.png")} style={styles.iconImage}></Image>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.routeCard} onPress={() =>
+                        this.props.navigation.navigate("DailyPic")
+                    }>
+                        <Text style={styles.routeText}>Daily Pictures</Text>
+                       
+                        <Image source={require("../assets/daily_pictures.png")} style={styles.iconImage}></Image>
+                    </TouchableOpacity>
+                </ImageBackground>
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1
+    },
+    droidSafeArea: {
+        marginTop: Platform.OS === "android" ? StatusBar.currentHeight : 0
+    },
+    backgroundImage: {
+        flex: 1,
+        resizeMode: 'cover',
+    },
+    routeCard: {
+        flex: 0.25,
+        marginLeft: 50,
+        marginRight: 50,
+        marginTop: 50,
+        borderRadius: 30,
+        backgroundColor: 'white'
+    },
+    titleBar: {
+        flex: 0.15,
+        justifyContent: "center",
+        alignItems: "center"
+    },
+    titleText: {
+        fontSize: 40,
+        fontWeight: "bold",
+        color: "white"
+    },
+    routeText: {
+        fontSize: 35,
+        fontWeight: "bold",
+        color: "black",
+        marginTop: 75,
+        paddingLeft: 30
+    },
+ 
+    
+    iconImage: {
+        position: "absolute",
+        height: 200,
+        width: 200,
+        resizeMode: "contain",
+        right: 20,
+        top: -80
+    }
+});
